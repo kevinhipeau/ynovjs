838738e917e61a4ed6746d3f064a5d3a1872defc
kevinhipeau <kevin.hipeau@gmail.COM>
2017-01-20 14:10:58 +0100

    ajout des tests

--- toto.js
+++ toto.js
@@ -2,7 +2,7 @@
 var expect=require('chai').expect;
 
 let prices = {banana:1, potato:2, tomato:3, cucumber:4, salad:5, apple:6};
-let products = ['tomato','cucumber','tomato','salad','potato','cucumber','potato','potato','tomato','potato'];
+let products = ['cucumber','cucumber','potato','cucumber'];
 
 var calc = require("./products").calc;
 describe('Hello world testing !',
@@ -10,9 +10,25 @@ describe('Hello world testing !',
 
 		it('should be true',function(){
 			var result =calc(prices,products);
+			expect(result.price).to.be.equal(10);
+			expect(result.countArticles).to.be.equal(4);
+			expect(result.countProducts).to.be.equal(2);
 
-			expect(result.price).to.be.equal(25);
-			expect(result.countArticles).to.be.equal(10);
-			expect(result.countProducts).to.be.equal(4);
+		})
+		it('should be true',function(){
+			var result =calc({},[]);
+			expect(result.price).to.be.equal(0);
+			expect(result.countArticles).to.be.equal(0);
+			expect(result.countProducts).to.be.equal(0);
 
-		})})
+		})
+		
+
+
+
+
+
+
+
+
+		});
